5e3d9f9437b69f16d5707c45f438ec0852148606
Nugo Bibilashvili <94049041+bibilashvili-nugo@users.noreply.github.com>
2024-09-30 04:09:47 -0700

    implement Course responsive

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -14,7 +14,10 @@ const Benefits = () => {
           title="Grab your desired one"
         />
 
-        <div className="flex flex-col md:flex-row flex-wrap gap-10 mb-10">
+        <div
+          className="flex flex-col  flex-wrap gap-10 mb-10 items-center sm:items-center
+        md:justify-center md:flex-row"
+        >
           {benefits.map((item) => (
             <div
               className=" block relative p-[7px] bg-no-repeat bg-[length:100%_100%] max-w-[24rem] border-color-1 border rounded-xl hover:border-transparent transition-all duration-300 ease-linear"
