acefeb6c7e2151d29ed66e2c815fa991860bfb9b
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-11-02 15:29:50 +0400

    make ourStat component dinamic

--- src/components/aboutPage/OurStats.jsx
+++ src/components/aboutPage/OurStats.jsx
@@ -1,13 +1,41 @@
 import SingleStat from "./SingleStat";
-// update to make dinamic
+
+const stats = [
+  {
+    id: "1",
+    value: "10+",
+    description: "years experience",
+  },
+
+  {
+    id: "2",
+    value: "99%",
+    description: "accurecy rate",
+  },
+  {
+    id: "3",
+    value: "500+",
+    description: "possitive reviews",
+  },
+  {
+    id: "4",
+    value: "600+",
+    description: "Trusted Patterns",
+  },
+];
 
 function OurStats() {
   return (
     <div className="grid grid-cols-[1fr_1fr] lg:grid-cols-[1fr_1fr_1fr_1fr] justify-items-center gap-y-[50px] mt-[50px] py-[50px] px-[120xp] lg:bg-gray-900 rounded-[10px]">
-      <SingleStat value="10+" description="years experience" />
-      <SingleStat value="99%" description="accurecy rate" />
-      <SingleStat value="500+" description="possitive reviewss" />
-      <SingleStat value="600+" description="Trusted Patterns" />
+      {stats.map((item) => {
+        return (
+          <SingleStat
+            key={item.id}
+            value={item.value}
+            description={item.description}
+          />
+        );
+      })}
     </div>
   );
 }
